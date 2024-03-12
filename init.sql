6e32e80672de779b27d335318f2f029434f8997f
jadd qiu <jadd@sf-express.com>
2024-03-12 16:38:07 +0800

    Other:

--- init.sql
+++ init.sql
@@ -3,4 +3,5 @@ insert into file (name, type, parent_dir, content) values ('2.txt', 'file', 0, '
 insert into file (name, type, parent_dir, content) values ('d1', 'directory', 0, '');
 insert into file (name, type, parent_dir, content) values ('sub1.txt', 'file', 3, 'subsub');
 insert into file (name, type, parent_dir, content) values ('sub2.txt', 'file', 3, 'subsub');
+insert into file (name, type, parent_dir, content) values ('sub3.txt', 'file', 3, 'sub3');
 
